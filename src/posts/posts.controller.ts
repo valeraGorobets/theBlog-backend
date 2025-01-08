f4e402296a468cfc0dd1b49bb6e3280e0ba5b306
Valeri_Gorobets <valera.gorobets@gmail.com>
2025-01-08 16:24:13 +0200

    test local db

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
-  constructor() {}
+	constructor(
+		private readonly postsService: PostsService,
+	) {
+	}
 
-  @Get()
-  getHello(): string {
-    return 'posts';
-  }
+	@Get()
+	async getHello(): Promise<object> {
+		return await this.postsService.getAll();
+	}
+
+	@Post()
+	async pushNew(): Promise<void> {
+		return await this.postsService.pushNew();
+	}
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Config, JsonDB } from 'node-json-db';
 
 @Injectable()
 export class PostsService {
-  getHello(): string {
-    return 'PostsService';
-  }
+	private db: JsonDB;
+
+	constructor() {
+		this.db = new JsonDB(new Config('./src/db/posts', true, false, '/'));
+	}
+
+	public async getAll(): Promise<object> {
+		return await this.db.getData('/');
+	}
+
+	public async pushNew(): Promise<void> {
+		return await this.db.push('/test1', new Date());
+	}
 }
