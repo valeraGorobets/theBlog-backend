2200078caea06c52fcd8208850946f6eda9f33e8
Valeri_Gorobets <valera.gorobets@gmail.com>
2025-01-09 16:07:00 +0200

    Bring back random 500 error

--- src/middlewares/random-error-middleware.middleware.ts
+++ src/middlewares/random-error-middleware.middleware.ts
@@ -6,7 +6,7 @@ export class RandomErrorMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
     const randomNumber = Math.random();
 
-    if (randomNumber < 0) {
+    if (randomNumber < 0.3) {
       throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     } else {
       next();
