5d7423dcbaec4aad1f945e94e8ca6582dde77bb4
Valeri_Gorobets <valera.gorobets@gmail.com>
2025-01-09 15:57:47 +0200

    add posts service and controller

--- src/controllers/posts/posts.controller.ts
+++ src/controllers/posts/posts.controller.ts
@@ -1,5 +1,50 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	NotFoundException,
+	Param,
+	Post,
+	Query, UseGuards,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { AuthGuard } from '../../guards/auth.guard';
+
+export class GetAllPostsQuery {
+	@IsOptional()
+	@Type(() => Number)
+	@IsNumber()
+	@Min(0)
+	page?: number;
+
+	@IsOptional()
+	@Type(() => Number)
+	@IsNumber()
+	@Min(0)
+	limit?: number;
+
+	constructor(page: number = 1, limit: number = 10) {
+		this.page = page;
+		this.limit = limit;
+	}
+}
+
+export class CreatePostDto {
+	@IsNotEmpty()
+	@IsString()
+	@MaxLength(30, { message: 'Title must not exceed 30 characters.' })
+	title: string;
+
+	@IsNotEmpty()
+	@IsString()
+	@MaxLength(500, { message: 'Text must not exceed 500 characters.' })
+	content: string;
+}
 
 @Controller('posts')
 export class PostsController {
@@ -9,12 +54,43 @@ export class PostsController {
 	}
 
 	@Get()
-	async getHello(): Promise<object> {
-		return await this.postsService.getAll();
+	@UsePipes(new ValidationPipe({ transform: true }))
+	async getAll(@Query() { page, limit }: GetAllPostsQuery): Promise<object[]> {
+		return await this.postsService.getRemainingPosts(page, limit);
 	}
 
+	@Get('recent')
+	async getRecent(): Promise<object[]> {
+		return await this.postsService.getRecentPosts();
+	}
+
+	@Get('amount')
+	async getAmount(): Promise<number> {
+		return await this.postsService.getRemainingPostsAmount();
+	}
+
+	@Get(':id')
+	async findOne(@Param('id') id: string): Promise<object> {
+		const post = await this.postsService.getPostById(id);
+		if (!post) {
+			throw new NotFoundException(`Item with ID ${id} not found.`);
+		}
+
+		return post;
+	}
+
+	@UseGuards(AuthGuard)
 	@Post()
-	async pushNew(): Promise<void> {
-		return await this.postsService.pushNew();
+	async create(@Body() createPostDto: CreatePostDto): Promise<object> {
+		return this.postsService.createNewPostObject(createPostDto);
+	}
+
+	@UseGuards(AuthGuard)
+	@Delete(':id')
+	async remove(@Param('id') id: string): Promise<void> {
+		const post = await this.postsService.deletePostById(id);
+		if (!post) {
+			throw new NotFoundException(`Item with ID ${id} not found.`);
+		}
 	}
 }
--- src/controllers/posts/posts.service.ts
+++ src/controllers/posts/posts.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Config, JsonDB } from 'node-json-db';
+import { CreatePostDto } from './posts.controller';
+import { faker } from '@faker-js/faker';
+
+const recentVisibleAmount: number = 3;
+const tags = ['design', 'technology', 'innovation', 'education', 'health', 'science', 'culture', 'travel', 'food', 'sports'];
 
 @Injectable()
 export class PostsService {
@@ -9,11 +14,80 @@ export class PostsService {
 		this.db = new JsonDB(new Config('./src/db/posts', true, false, '/'));
 	}
 
-	public async getAll(): Promise<object> {
-		return await this.db.getData('/');
+	public async getRemainingPosts(page: number, limit: number): Promise<object[]> {
+		const startIndex = (page - 1) * limit;
+		const endIndex = page * limit;
+		return (await this.getAll())
+			.slice(recentVisibleAmount)
+			.slice(startIndex, endIndex);
 	}
 
-	public async pushNew(): Promise<void> {
-		return await this.db.push('/test1', new Date());
+	public async getRemainingPostsAmount(): Promise<number> {
+		return await this.getPostsAmount() - recentVisibleAmount;
+	}
+
+	public async getRecentPosts(): Promise<object[]> {
+		return (await this.getAll()).slice(0, recentVisibleAmount);
+	}
+
+	public async getPostById(id: string): Promise<object> {
+		const index = await this.db.getIndex('/posts', id);
+		if (index === -1) {
+			return null;
+		}
+		return this.db.getData(`/posts[${ index }]`);
+	}
+
+	public async createNewPostObject({ title, content }: CreatePostDto): Promise<object> {
+		const newPost = {
+			id: await this.getNextId(),
+			tittttttle: title,
+			content: content,
+			tags: this.getRandomTags(),
+			the_real_date_of_creation: new Date().toISOString(),
+			author_name: faker.person.fullName(),
+			img: 'https://picsum.photos/600/300',
+		};
+
+		await this.db.push('/posts[]', newPost, true);
+
+		return newPost;
+	}
+
+	public async deletePostById(id: string): Promise<boolean> {
+		const index = await this.db.getIndex('/posts', id);
+		if (index === -1) {
+			return null;
+		}
+		await this.db.delete(`/posts[${ index }]`);
+		return true;
+	}
+
+	private async getPostsAmount(): Promise<number> {
+		const posts = await this.getAll();
+		return posts
+			.map((p: any) => p.id)
+			.sort((a, b) => b - a)[0];
+	}
+
+	private async getAll(): Promise<object[]> {
+		const allPosts = (await this.db.getData('/') as { posts: any[] }).posts;
+		return allPosts.sort((a, b) => {
+			return new Date(b.the_real_date_of_creation).getTime() - new Date(a.the_real_date_of_creation).getTime();
+		});
+	}
+
+	private async getNextId(): Promise<string> {
+		const latest = await this.getPostsAmount();
+		return (+latest + 1).toString();
+	}
+
+	private getRandomTags(): string[] {
+		for (let i = tags.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[tags[i], tags[j]] = [tags[j], tags[i]];
+		}
+		const numberOfTags = Math.floor(Math.random() * 3) + 1;
+		return tags.slice(0, numberOfTags);
 	}
 }
