8fef6306b24f8b56b18dcdb600651c31062fbb8e
Valeri_Gorobets <valera.gorobets@gmail.com>
2025-01-09 12:44:06 +0200

    add auth validation

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,14 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 import { AuthService } from './auth.service';
 
 export class AuthDto {
   @IsEmail()
   @IsNotEmpty()
+  @Matches(/^.+@\S+\.\S+$/, {
+    message: 'Invalid email format',
+  })
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @Matches(/^(?=.*[A-Z])(?=.*[<>{}()\[\]!@#$%^&*]).{6,}$/, {
+    message: 'Password must contain at least 1 uppercase letter, 1 special character from the set <>{}()[]!@#$%^&*, and be at least 6 symbols long.',
+  })
   password: string;
 }
 
